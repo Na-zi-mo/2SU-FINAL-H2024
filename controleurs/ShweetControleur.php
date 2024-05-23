5ab934f61fa5f31278c0e2f924b47580ea0d9423
Na-zi-mo <nazimad2004@gmail.com>
2024-05-23 12:15:21 -0400

    correction de bugs pour la fonctionnalité supprimer et début d'implémentation de la fonctionnalté commenter

--- controleurs/ShweetControleur.php
+++ controleurs/ShweetControleur.php
@@ -57,47 +57,103 @@ class ShweetControleur extends BaseControleur
         echo $vue->generer();
     }
 
-    function supprimer(): void
+    function commenter(): void
     {
         $utilisateurConnecte = $this->getUtilisateurConnecte();
-        if (isset($_POST['shweet-id']))
+        if (isset($_POST['parent-id']))
         {
-
-            $shweet = $this->shweetDao->select($_POST['shweet-id']);
-            if ($utilisateurConnecte->getId() == $shweet->getAuteurId())
+            $shweetParent = $this->shweetDao->select($_POST['parent-id']);
+            if (isset($_POST['profil-origine-id']))
             {
-                if (isset($_POST['profil-origine-id']))
+                $vue = new CreateurVue('vues/profil.phtml');
+
+                $shweet = $this->shweetDao->select($_POST['shweet-id']);
+
+                $currentUser = null;
+                if (is_null($shweet->getParentId()))
                 {
-                    $vue = new CreateurVue('vues/profil.phtml');
-
-                    $shweet = $this->shweetDao->select($_POST['shweet-id']);
-
-                    $currentUser = null;
-                    if (is_null($shweet->getParentId()))
-                    {
-                        $currentUser = $this->utilisateurDao->select($shweet->getAuteurId());
-                    }
-                    else
-                    {
-                        $currentUser = $this->utilisateurDao->select($shweet->getParentId()?->getAuteurId());
-                    }
-
-                    $this->shweetDao->delete($_POST['shweet-id']);
-                    $vue->assigner('utilisateur', $currentUser);
-                    $vue->assigner('shweets', $this->shweetDao->selectDerniersShweetsParents($currentUser->getId()));
-                    echo $vue->generer();
+                    $currentUser = $this->utilisateurDao->select($shweet->getAuteurId());
                 }
                 else
                 {
-                    $vue = new CreateurVue('vues/accueil.phtml');
-                    $this->shweetDao->delete($_POST['shweet-id']);
-                    $vue->assigner('shweets', $this->shweetDao->selectDerniersShweetsParents());
+                    $currentUser = $this->utilisateurDao->select($shweet->getParentId()?->getAuteurId());
+                }
+
+                $this->shweetDao->delete($_POST['shweet-id']);
+                $vue->assigner('utilisateur', $currentUser);
+                $vue->assigner('shweets', $this->shweetDao->selectDerniersShweetsParents($currentUser->getId()));
+                echo $vue->generer();
+            }
+            else
+            {
+                $vue = new CreateurVue('vues/accueil.phtml');
+                $this->shweetDao->delete($_POST['shweet-id']);
+                $vue->assigner('shweets', $this->shweetDao->selectDerniersShweetsParents());
+                echo $vue->generer();
+            }
+        }
+        else
+        {
+            $erreurs[] = "Un shweet doit parent être choisi pour être commenter.";
+            $vue = new CreateurVue('vues/profil.phtml');
+            $vue->assigner('utilisateur', $utilisateurConnecte);
+            $vue->assigner('erreurs', $erreurs);
+            echo $vue->generer();
+        }
+    }
+
+    function supprimer(): void
+    {
+        $utilisateurConnecte = $this->getUtilisateurConnecte();
+        if (isset($utilisateurConnecte))
+        {
+            if (isset($_POST['shweet-id']))
+            {
+
+                $shweet = $this->shweetDao->select($_POST['shweet-id']);
+                if ($utilisateurConnecte->getId() == $shweet->getAuteurId())
+                {
+                    if (isset($_POST['profil-origine-id']))
+                    {
+                        $vue = new CreateurVue('vues/profil.phtml');
+
+                        $shweet = $this->shweetDao->select($_POST['shweet-id']);
+
+                        $this->shweetDao->delete($_POST['shweet-id']);
+                        $vue->assigner('utilisateur', $this->utilisateurDao->select($_POST['profil-origine-id']));
+                        $vue->assigner('shweets', $this->shweetDao->selectDerniersShweetsParents($_POST['profil-origine-id']));
+                        echo $vue->generer();
+                    }
+                    else
+                    {
+                        $vue = new CreateurVue('vues/accueil.phtml');
+                        $this->shweetDao->delete($_POST['shweet-id']);
+                        $vue->assigner('shweets', $this->shweetDao->selectDerniersShweetsParents());
+                        echo $vue->generer();
+                    }
+                }
+                else
+                {
+                    $erreurs[] = "Il faut être auteur d'un shweet pour pouvoir le supprimer.";
+                    $vue = null;
+                    if (isset($_POST['profil-origine-id']))
+                    {
+                        $vue = new CreateurVue('vues/profil.phtml');
+                        $vue->assigner('utilisateur', $utilisateurConnecte);
+                        $vue->assigner('erreurs', $erreurs);
+                    }
+                    else
+                    {
+                        $vue = new CreateurVue('vues/accueil.phtml');
+                        $vue->assigner('shweets', $this->shweetDao->selectDerniersShweetsParents());
+                        $vue->assigner('erreurs', $erreurs);
+                    }
                     echo $vue->generer();
                 }
             }
             else
             {
-                $erreurs[] = "Il faut être auteur d'un shweet pour pouvoir le supprimer.";
+                $erreurs[] = "Un shweet doit être choisi pour être supprimé.";
                 $vue = new CreateurVue('vues/profil.phtml');
                 $vue->assigner('utilisateur', $utilisateurConnecte);
                 $vue->assigner('erreurs', $erreurs);
@@ -106,9 +162,9 @@ class ShweetControleur extends BaseControleur
         }
         else
         {
-            $erreurs[] = "Un shweet doit être choisi pour être supprimé.";
-            $vue = new CreateurVue('vues/profil.phtml');
-            $vue->assigner('utilisateur', $utilisateurConnecte);
+            $erreurs[] = "Il faut être connecté pour supprimer un shweet.";
+            $vue = new CreateurVue('vues/accueil.phtml');
+            $vue->assigner('shweets', $this->shweetDao->selectDerniersShweetsParents());
             $vue->assigner('erreurs', $erreurs);
             echo $vue->generer();
         }
